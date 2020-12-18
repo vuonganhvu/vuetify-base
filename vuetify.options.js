311c1ba84c6335d90e339307259016bcba419e36
Macbook <macbook@Macbookui-MacBookPro.local>
2020-12-18 14:21:26 +0700

    Initial commit

---
+++ vuetify.options.js
@@ -0,0 +1,21 @@
+export default function ({app}) {
+  return {
+    breakpoint: {},
+    icons: {},
+    lang: {},
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  }
+}
